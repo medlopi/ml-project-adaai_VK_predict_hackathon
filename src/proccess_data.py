bc3c7231641675257311e6d367655407c941736b
Arseniy Pozdeyev <pozdeyevarseniy@gmail.com>
2026-05-18 13:28:17 +0300

    add sampler func

---
+++ src/proccess_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def get_sample_data(path, part_size):
+    df = pd.read_csv(f'../{path}')
+    zero_class = df[df['target'] == 0]
+    one_class = df[df['target'] == 1]
+
+    n_zero = len(zero_class)
+    n_one = len(one_class)
+
+    zero_sample = zero_class.sample(int(n_zero * part_size))
+    one_sample = one_class.sample(int(n_one * part_size))
+
+    res_sample = pd.concat([zero_sample, one_sample], ignore_index=True)
+    save_path = '../data/small_sample.csv'
+    res_sample.to_csv(save_path)
+
+    return res_sample
+
+
